ffa1ffbb1e090c275f57e1438bf917d0587e47ff
Salim Absi <salim.absi.me@gmail.com>
2025-11-20 14:01:59 +0300

    feat: add not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+export default function NotFound() {
+    return (
+        <div className="text-center py-20">
+            <p className="text-neutral-600 dark:text-neutral-300">
+                404 - Page Not Found
+            </p>
+        </div>
+    )
+}
