e7e4940ebb01e2c6127d667549e456c4fb35d3ca
Salim Absi <salim.absi.me@gmail.com>
2025-11-29 21:38:17 +0300

    refactor: default to dark mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,7 +43,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       >
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
+          defaultTheme="dark"
           enableSystem
           disableTransitionOnChange
         >
